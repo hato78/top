f4618b54ae81e15bd429ce0d98ab526fb028cb6e
uxcat <101016018+hato78@users.noreply.github.com>
2025-03-05 16:36:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-    type: 'loop',  // ループスライダーを有効にする
-    perPage: 1,    // 1枚ずつ表示
-    focus: 'center', // 中央に配置
-    padding: '5rem', // 左右の余白を適度に調整
-    start: 0, // スタート位置を1枚目にする
-    trimSpace: false, // 自動トリミングを無効にする
-  }).mount();
-});
+var splide = new Splide( '.splide', {
+  direction: 'ttb',
+  height   : '10rem',
+} );
+
+splide.mount();
